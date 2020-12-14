e1d33fc8b26915cd0e02c03199414531a188406d
k.khoruzhenko <khoruzhenko.work@gmail.com>
2020-12-14 23:49:58 +0300

    format duration

--- productivity_timer/lib/timer.dart
+++ productivity_timer/lib/timer.dart
@@ -6,4 +6,14 @@ class CountDownTimer {
   Timer timer;
   Duration _time;
   Duration _fullTime;
+
+  String returnTime(Duration t) {
+    String minutes = (t.inMinutes < 10)
+        ? '0' + t.inMinutes.toString()
+        : t.inMinutes.toString();
+    int numSeconds = t.inSeconds - (t.inMinutes * 60);
+    String seconds =
+        (numSeconds < 10) ? '0' + numSeconds.toString() : numSeconds.toString();
+    return minutes + ':' + seconds;
+  }
 }
