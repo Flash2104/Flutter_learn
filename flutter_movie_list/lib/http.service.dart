828b07a03bb4630e5e1894e33e07f7126dca8363
k.khoruzhenko <khoruzhenko.work@gmail.com>
2020-12-24 23:39:06 +0300

    movie_list init

---
+++ flutter_movie_list/lib/http.service.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class MovieHttpProxy {
+  final String _urlApiKey = 'api_key=c7d7585faad21b08a085bd6dde6cf0ce';
+  final String _urlBase = 'https://api.themoviedb.org/3/movie';
+  final String _urlUpcoming = '/upcoming';
+  final String _urlLanguage = '&language=en-US';
+
+  Future<String> getUpcoming() async {
+    final String upcoming = _urlBase + _urlUpcoming + _urlApiKey + _urlLanguage;
+    http.Response result = await http.get(upcoming);
+    if (result.statusCode == HttpStatus.ok) {
+      String responseBody = result.body;
+      return responseBody;
+    } else {
+      return null;
+    }
+  }
+}
